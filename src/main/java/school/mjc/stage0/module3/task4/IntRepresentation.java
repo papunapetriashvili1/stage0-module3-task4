75ee83515175e29f0a10c723e11826758585e101
papuna <papunapetriashvili1@gmail.com>
2023-12-23 19:09:11 +0400

    final

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = 99;
+        int h = 104;
+        int a = 97;
+        int r = 114;
+
+        char charC = (char) c;
+        char charH = (char) h;
+        char charA = (char) a;
+        char charR = (char) r;
+
+        System.out.println(charC);
+        System.out.println(charH);
+        System.out.println(charA);
+        System.out.println(charR);
     }
 }
